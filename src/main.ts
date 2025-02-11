e0aa7d9fd57732aef9373cfbbe4a515899caa697
Mykola <Pavliust Mykola>
2025-02-11 16:04:51 +0200

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('example')
+    .setDescription('The cats description')
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'header',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      docExpansion: 'list',
+      defaultModelsExpandDepth: 2,
+      persistAuthorization: true,
+    },
+  });
+
+  await app.listen(3000, '0.0.0.0', () => {
+    console.log('Server running on http://localhost:3000');
+    console.log('Swagger running on http://localhost:3000/docs');
+  });
 }
-bootstrap();
+void bootstrap();

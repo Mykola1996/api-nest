19dfe879b423b3d2908d6544635cf853c248f9a9
mykola <pavliustmykola1996@gmail.com>
2025-02-13 12:37:29 +0200

    validation/transform

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('example')
-    .setDescription('The cats description')
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
     .setVersion('1.0.0')
     .addBearerAuth({
       type: 'http',
@@ -24,6 +26,13 @@ async function bootstrap() {
       persistAuthorization: true,
     },
   });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(3000, '0.0.0.0', () => {
     console.log('Server running on http://localhost:3000');

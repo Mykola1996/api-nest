cdfd8c790df4e5cd3b61b27b94d5eed6e2c44e1e
user <pavliustmykola@gmail.com>
2025-02-24 19:46:06 +0200

    migration

--- src/database/entities/user.entity.ts
+++ src/database/entities/user.entity.ts
@@ -1,1 +1,25 @@
-export class UserEntity {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  age: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  isActive: boolean;
+}
